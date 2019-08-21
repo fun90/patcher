7091ac54cbd112dc9be9d71233844f14a06a7be3
fun90 <congzi@live.com>
2019-08-21 14:44:31 +0800

    支持IDEA2019.2

--- src/com/fun90/idea/patcher/ClassesExportAction.java
+++ src/com/fun90/idea/patcher/ClassesExportAction.java
@@ -1,15 +1,12 @@
 package com.fun90.idea.patcher;
 
-import com.intellij.openapi.actionSystem.AnAction;
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.DataKeys;
+import com.intellij.openapi.actionSystem.*;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.roots.CompilerModuleExtension;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.vfs.VirtualFileManager;
-import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiJavaFile;
 import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.lang.StringUtils;
@@ -24,10 +21,10 @@ public class ClassesExportAction extends AnAction {
     @Override
     public void actionPerformed(AnActionEvent e) {
         try {
-            com.intellij.openapi.actionSystem.DataContext dataContext = e.getDataContext();
-            PsiJavaFile javaFile = (PsiJavaFile) ((PsiFile) DataKeys.PSI_FILE.getData(dataContext)).getContainingFile();
+            DataContext dataContext = e.getDataContext();
+            PsiJavaFile javaFile = (PsiJavaFile) LangDataKeys.PSI_FILE.getData(dataContext).getContainingFile();
             String sourceName = javaFile.getName();
-            Module module = (Module) DataKeys.MODULE.getData(dataContext);
+            Module module = LangDataKeys.MODULE.getData(dataContext);
             String compileRoot = CompilerModuleExtension.getInstance(module).getCompilerOutputPath().getPath();
             getVirtualFile(sourceName, CompilerModuleExtension.getInstance(module).getCompilerOutputPath().getChildren(), compileRoot);
             VirtualFileManager.getInstance().syncRefresh();
@@ -45,10 +42,11 @@ public class ClassesExportAction extends AnAction {
             for (int i$ = 0; i$ < len$; i$++) {
                 VirtualFile vf = arr$[i$];
                 String srcName;
-                if (StringUtils.indexOf(vf.toString(), "$") != -1)
+                if (StringUtils.indexOf(vf.toString(), "$") != -1) {
                     srcName = StringUtils.substring(vf.toString(), StringUtils.lastIndexOf(vf.toString(), "/") + 1, StringUtils.indexOf(vf.toString(), "$"));
-                else
+                } else {
                     srcName = StringUtils.substring(vf.toString(), StringUtils.lastIndexOf(vf.toString(), "/") + 1, StringUtils.length(vf.toString()) - 6);
+                }
                 String dstName = StringUtils.substring(sourceName, 0, StringUtils.length(sourceName) - 5);
                 if (StringUtils.equals(srcName, dstName)) {
                     String outRoot = (new StringBuilder()).append(StringUtils.substring(compileRoot, 0, StringUtils.lastIndexOf(compileRoot, "/"))).append("/out").toString();
